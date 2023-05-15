be6f8a716318f3da8d430cc94fcd68a2e74cecf1
Richard Steinmetz <richard@steinmetz.cloud>
2023-05-15 11:27:42 +0200

    chore: fix frontend tests
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -3,6 +3,7 @@
  *
  * @author Michael Blumenstein <M.Flower@gmx.de>
  * @author 2022 Christoph Wurst <christoph@winzerhof-wurst.at>
+ * @author 2023 Richard Steinmetz <richard@steinmetz.cloud>
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -20,7 +21,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require('jsdom-global')()
+// Emulate browser environment for tests (inside node)
+// Ref (url): https://github.com/jsdom/jsdom/issues/2383#issuecomment-442199291
+// Ref (SVGElement): https://github.com/vuejs/core/issues/3590
+require('jsdom-global')('', {
+	url: 'http://localhost',
+})
+global.SVGElement = window.SVGElement
 
 const t = (app, str) => str
 
