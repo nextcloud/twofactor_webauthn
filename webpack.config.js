654dfd7b40955e4111461b7fd15af00a52bee3a3
Richard Steinmetz <richard@steinmetz.cloud>
2025-04-03 22:26:13 +0200

    feat: migrate to vue 3
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
 delete webpackConfig.entry['main']
-webpackConfig.entry['challenge'] = path.join(__dirname, 'src', 'main-challenge.js')
+//webpackConfig.entry['challenge'] = path.join(__dirname, 'src', 'main-challenge.js')
 webpackConfig.entry['settings'] = path.join(__dirname, 'src', 'main-settings.js')
-webpackConfig.entry['login-setup'] = path.join(__dirname, 'src', 'main-login-setup.js')
+//webpackConfig.entry['login-setup'] = path.join(__dirname, 'src', 'main-login-setup.js')
 
 module.exports = webpackConfig
