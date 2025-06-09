50414408b53b57a18bf48e3220e602bfbd06a79c
Richard Steinmetz <richard@steinmetz.cloud>
2025-06-09 17:32:28 +0200

    chore(tests): cleanup bootstrap.php to be forward-compatible
    Ref https://github.com/nextcloud/server/pull/52945
    
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,8 +7,11 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+use OCP\App\IAppManager;
+use OCP\Server;
+
 require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
-OC_App::loadApp('twofactor_webauthn');
+Server::get(IAppManager::class)->loadApp('twofactor_webauthn');
